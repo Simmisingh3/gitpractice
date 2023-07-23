0493d425be30e42ea64cc84cfbb0a0a077b5dcdc
SIMMI <simmi03singh@gmail.com>
2023-07-23 23:07:40 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-def addition(a,b,c):
-    print(a+b+c)
-=======
 ## addition code
 def addition(a,b,c):
->>>>>>> df92c8c64124fb29cae10dbcbf1e0f8c3fcf458c
     return a+b+c
 
 def multiplication(a,b):
